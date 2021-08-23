45e68a328975b3a9df6af1090d23c44ed69e3075
Kamil Abdulatipov <abdulatipov.k97@gmail.com>
2021-08-23 17:51:24 +0300

    init project

---
+++ app/src/main/java/hach/dev/samplejetpackcompose/MainActivity.kt
@@ -0,0 +1,11 @@
+package hach.dev.samplejetpackcompose
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
